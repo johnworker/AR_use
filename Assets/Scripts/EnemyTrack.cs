f380eaff64b4723f4cd17851ab16b5179761c6d1
hunte <besscomdark@gmail.com>
2023-01-04 19:44:16 +0800

    調整

---
+++ Assets/Scripts/EnemyTrack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrack : MonoBehaviour
+{
+    public float speed;
+    public float radius;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
